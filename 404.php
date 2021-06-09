6abbb983d741361087268939d1b66545d94736b4
EZunigaF <estebanzfl@gmail.com>
2021-06-08 22:51:37 -0600

    page_About

--- 404.php
+++ 404.php
@@ -13,4 +13,6 @@
     <div class="tenor-gif-embed" data-postid="14382042" data-share-method="host" data-width="100%" data-aspect-ratio="1.3351206434316354"><a href="https://tenor.com/view/ostrich-bird-framesequence-framesequence-gifs-petter-pentila-gif-14382042">Ostrich Bird GIF</a> from <a href="https://tenor.com/search/ostrich-gifs">Ostrich GIFs</a></div><script type="text/javascript" async src="https://tenor.com/embed.js"></script>
 </div>
 
+
+
 <?php get_footer(); ?>
---
+++ page-about.php
@@ -0,0 +1,50 @@
+<?php get_header(); ?>
+
+
+<div class="offset-1">
+    <div class="p-2">
+        <div class="pt-5 display-1">
+            Equipo de Desarrollo
+        </div>
+    </div>
+
+    <div class="container-fluid pt-5 pb-5">
+        <div class="row">
+            <div class="col-11 col-md-5">
+                <img class="img-about"
+                    src="<?php echo get_template_directory_uri() . "/imgs/about/ceo.jpg"; ?>"
+                    alt="servicio">
+            </div>
+            <div class="col-11 col-md-7">
+                <div class="display-2 about-member pb-2 pt-2">
+                    <p>CEO</p>
+                </div>
+                <div class="about-text">
+                    Co-fundador de Pesa Marketing. <br> <p class="display-5">Esteban José Zúñiga Fallas</p> <br> Informático y Tecnólogo Multimedia <br> Graduado de la UCR en Informática y Tecnología Multimedia
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid pt-5 pb-5">
+        <div class="row">
+            
+            <div class="col-11 col-md-7">
+                <div class="display-2 about-member pb-2 pt-2">
+                    <p>Community Manager</p>
+                </div>
+                <div class="about-text pb-3">
+                    Co-fundador de Pesa Marketing. <br> <p class="display-5">Andres Zúñiga Fallas </p> <br> Marketero Digital <br> Estudiante ST Angel. <BR> Especialista en publicidad en redes sociales.
+                </div>
+            </div>
+
+            <div class="col-11 col-md-5">
+                <img class="img-about"
+                    src="<?php echo get_template_directory_uri() . "/imgs/about/manager.png"; ?>"
+                    alt="servicio">
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
