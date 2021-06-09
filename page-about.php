562dc40bdba6fac5112911af5b2b3102b9103cce
EZunigaF <estebanzfl@gmail.com>
2021-06-09 08:57:00 -0600

    ceo img

--- page-about.php
+++ page-about.php
@@ -12,7 +12,7 @@
         <div class="row">
             <div class="col-11 col-md-5">
                 <img class="img-about"
-                    src="<?php echo get_template_directory_uri() . "/imgs/about/ceo.jpg"; ?>"
+                    src="<?php echo get_template_directory_uri() . "/imgs/about/ceo.png"; ?>"
                     alt="miembro_ceo">
             </div>
             <div class="col-11 col-md-7">
